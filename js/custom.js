f250bb4edb1e2f5c84c2093039d2ba1131405730
sakaethossain <sakaethossain122@gmail.com>
2022-01-06 19:30:13 +0600

    scss responsive site

---
+++ js/custom.js
@@ -0,0 +1,76 @@
+$(function () {
+  //    sticky navbar
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+    var sticky = $(".sticky-top");
+    if (scrolling >= 110) {
+      sticky.addClass("navbg");
+    } else {
+      sticky.removeClass("navbg");
+    }
+  });
+
+  //==== type js ====//
+
+  $(".typed").typed({
+    strings: ["new realtime interactive reality to your web.", "income increase from advertising ads.", "interactive online tool to immediately boost your CTA."],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 30,
+    // time before typing starts
+    startDelay: 400,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: 5,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: "html",
+    // call when done callback function
+    callback: function () {},
+    // starting callback function before each string
+    preStringTyped: function () {},
+    //callback for every typed string
+    onStringTyped: function () {},
+    // callback for reset
+    resetCallback: function () {},
+  });
+
+  //==== webs-tabs part js ====//
+
+  $(".tabs-nav a").click(function () {
+    // Check for active
+    $(".tabs-nav li").removeClass("active");
+    $(this).parent().addClass("active");
+
+    // Display active tab
+    let currentTab = $(this).attr("href");
+    $(".tabs-content div").hide();
+    $(currentTab).show();
+
+    return false;
+  });
+
+  // toggle navbar button //
+  $(".toggle-navbar").on("click", function () {
+    $(".nav-menu-left").toggleClass("showing");
+  });
+
+  /*** Vid musklick på länk e mneyn så plockas classen showing bort***/
+  $(".nav-menu-left li").on("click", function () {
+    $(".nav-menu-left").removeClass("showing");
+  });
+  if ($(this).scrollbottom() > 10) {
+    $(".nav-menu-left").removeClass("showing");
+  }
+});
