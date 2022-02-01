85577a01fbf67b2f614334e85b83e8eb9865fe01
sakaethossain <sakaethossain122@gmail.com>
2022-02-02 02:30:18 +0600

    sdsf

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,7 @@ $(function () {
       sticky.removeClass("navbg");
     }
   });
-
   //==== type js ====//
-
   $(".typed").typed({
     strings: ["real-time interaction to web", "income increase from advertising", "immediate boost of Call to action"],
     // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
@@ -47,17 +45,14 @@ $(function () {
   });
 
   //==== webs-tabs part js ====//
-
   $(".tabs-nav a").click(function () {
     // Check for active
     $(".tabs-nav li").removeClass("active");
     $(this).parent().addClass("active");
-
     // Display active tab
     let currentTab = $(this).attr("href");
     $(".tabs-content div").hide();
     $(currentTab).show();
-
     return false;
   });
 
@@ -65,7 +60,6 @@ $(function () {
   $(".toggle-navbar").on("click", function () {
     $(".responsive-menu").toggleClass("showing");
   });
-
   /*** Vid musklick på länk e mneyn så plockas classen showing bort***/
   $(".responsive-menu li").on("click", function () {
     $(".responsive-menu").removeClass("showing");
@@ -74,3 +68,31 @@ $(function () {
     $(".responsive-menu").removeClass("showing");
   }
 });
+// pricing part js
+function check() {
+  var checkBox = document.getElementById("switch");
+  var monthly_price = document.getElementsByClassName("monthly_price");
+  var yearly_price = document.getElementsByClassName("yearly_price");
+  var monthly = document.getElementsByClassName("monthly");
+  var yearly = document.getElementsByClassName("yearly");
+
+  for (var i = 0; i < monthly_price.length; i++) {
+    if (checkBox.checked == true) {
+      monthly_price[i].style.display = "none";
+      yearly_price[i].style.display = "inline";
+    } else if (checkBox.checked == false) {
+      monthly_price[i].style.display = "inline";
+      yearly_price[i].style.display = "none";
+    }
+  }
+  for (var i = 0; i < monthly.length; i++) {
+    if (checkBox.checked == true) {
+      monthly[i].style.color = "#b6babb";
+      yearly[i].style.color = "#5337c8";
+    } else if (checkBox.checked == false) {
+      monthly[i].style.color = "#5337c8";
+      yearly[i].style.color = "#b6babb";
+    }
+  }
+}
+check();
